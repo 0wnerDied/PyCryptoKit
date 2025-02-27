0990038e4009c5d940f13de600d2dd72a4229d30
0wnerDied <z1281552865@gmail.com>
2025-02-27 15:15:08 +0800

    hash rename
    Signed-off-by: 0wnerDied <z1281552865@gmail.com>

--- core/hash/__init__.py
+++ core/hash/__init__.py
@@ -16,36 +16,38 @@ from typing import Dict, Type, Any, List, Tuple
 from .base import HashBase
 from .md5 import MD5Hash
 from .sha import SHA1Hash, SHA224Hash, SHA256Hash, SHA384Hash, SHA512Hash
-from .sha3 import SHA3_224Hash, SHA3_256Hash, SHA3_384Hash, SHA3_512Hash
+from .sha3 import SHA3_224Hash, SHA3_256Hash, SHA3_384Hash, SHA3_512Hash, SHAKE128Hash, SHAKE256Hash
 from .blake import BLAKE2bHash, BLAKE2sHash
 from .sm3 import SM3Hash
 
 # 算法注册表
 # 格式: {算法名称: (算法类, 是否安全, 描述, 默认参数)}
 HASH_ALGORITHMS: Dict[str, Tuple[Type[HashBase], bool, str, Dict[str, Any]]] = {
-    "md5": (MD5Hash, False, "MD5 消息摘要算法 (不安全，仅用于兼容)", {}),
-    "sha1": (SHA1Hash, False, "SHA-1 安全哈希算法 (不安全，仅用于兼容)", {}),
-    "sha224": (SHA224Hash, True, "SHA-224 安全哈希算法", {}),
-    "sha256": (SHA256Hash, True, "SHA-256 安全哈希算法", {}),
-    "sha384": (SHA384Hash, True, "SHA-384 安全哈希算法", {}),
-    "sha512": (SHA512Hash, True, "SHA-512 安全哈希算法", {}),
-    "sha3_224": (SHA3_224Hash, True, "SHA3-224 安全哈希算法", {}),
-    "sha3_256": (SHA3_256Hash, True, "SHA3-256 安全哈希算法", {}),
-    "sha3_384": (SHA3_384Hash, True, "SHA3-384 安全哈希算法", {}),
-    "sha3_512": (SHA3_512Hash, True, "SHA3-512 安全哈希算法", {}),
-    "blake2b": (
+    "MD5": (MD5Hash, False, "MD5 消息摘要算法 (不安全，仅用于兼容)", {}),
+    "SHA1": (SHA1Hash, False, "SHA-1 安全哈希算法 (不安全，仅用于兼容)", {}),
+    "SHA224": (SHA224Hash, True, "SHA-224 安全哈希算法", {}),
+    "SHA256": (SHA256Hash, True, "SHA-256 安全哈希算法", {}),
+    "SHA384": (SHA384Hash, True, "SHA-384 安全哈希算法", {}),
+    "SHA512": (SHA512Hash, True, "SHA-512 安全哈希算法", {}),
+    "SHA3-224": (SHA3_224Hash, True, "SHA3-224 安全哈希算法", {}),
+    "SHA3-256": (SHA3_256Hash, True, "SHA3-256 安全哈希算法", {}),
+    "SHA3-384": (SHA3_384Hash, True, "SHA3-384 安全哈希算法", {}),
+    "SHA3-512": (SHA3_512Hash, True, "SHA3-512 安全哈希算法", {}),
+    "SHAKE-128": (SHAKE128Hash, True, "SHAKE128 安全哈希算法", {}),
+    "SHAKE-256": (SHAKE256Hash, True, "SHAKE256 安全哈希算法", {}),
+    "BLAKE2b": (
         BLAKE2bHash,
         True,
         "BLAKE2b 哈希算法",
         {"digest_size": 64, "key": b"", "salt": b"", "person": b""},
     ),
-    "blake2s": (
+    "BLAKE2s": (
         BLAKE2sHash,
         True,
         "BLAKE2s 哈希算法",
         {"digest_size": 32, "key": b"", "salt": b"", "person": b""},
     ),
-    "sm3": (SM3Hash, True, "SM3 国密哈希算法", {}),
+    "SM3": (SM3Hash, True, "SM3 国密哈希算法", {}),
 }
 
 # 导出所有算法名称，按安全性分组
